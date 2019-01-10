61c7558e600bb5321f970d3c9d0bd6e02d50b32f
Kenny Ho <ken.ho1996@hotmail.com>
2019-01-10 11:14:12 -0500

    Initial Commit

---
+++ Kinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kinder
+//
+//  Created by Kenny Ho on 1/10/19.
+//  Copyright © 2019 Kenny Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
